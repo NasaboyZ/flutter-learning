a80658c363a26f307ec3fa5f14691422934151c2
NazaboyZ <josefleite.00@hotmail.com>
2025-09-05 15:00:17 +0200

    final and const big differnc and use

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,32 +1,6 @@
+import 'package:first_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
-}
-
-class GradientContainer extends StatelessWidget {
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            const Color.fromARGB(174, 72, 53, 219),
-            const Color.fromARGB(170, 57, 151, 228),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'josef',
-          style: TextStyle(
-            color: Color.fromARGB(255, 241, 241, 241),
-            fontSize: 28.0,
-          ),
-        ),
-      ),
-    );
-  }
+  runApp(MaterialApp(home: Scaffold(body: GradientContainer()),),);
 }
