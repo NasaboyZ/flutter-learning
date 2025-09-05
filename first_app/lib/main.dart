dc378c92c5c4daaca09657b901c3630e92e5c6d5
NazaboyZ <josefleite.00@hotmail.com>
2025-09-05 15:38:46 +0200

    reusable widget and using multiple constructor function

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -2,5 +2,14 @@ import 'package:first_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: GradientContainer()),),);
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          const Color.fromARGB(184, 32, 52, 203),
+          const Color.fromARGB(186, 39, 163, 240),
+        ),
+      ),
+    ),
+  );
 }
