7c60481c46d78906ccbd4d8e224a23f59814e47e
NazaboyZ <josefleite.00@hotmail.com>
2025-09-05 17:31:26 +0200

    roller dice app is done from screatch

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/dice_roller.dart';
 import 'package:flutter/material.dart';
 
 const startAlignment = Alignment.topLeft;
@@ -7,11 +8,6 @@ class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
   final Color color1;
   final Color color2;
-
-  void rollDice() {
-    //function what Todo
-    print('jello');
-  }
 
   @override
   Widget build(context) {
@@ -23,24 +19,7 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset('assets/images/dice-2.png', width: 200),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(fontSize: 28.0),
-                // padding: const EdgeInsets.only(top: 20.0),
-              ),
-              child: const Text('Roll Dice'),
-            ),
-          ],
-        ),
-      ),
+      child: Center(child: DiceRoller()),
     );
   }
 }
